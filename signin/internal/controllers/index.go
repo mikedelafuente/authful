6ce9f49326cbd47a4907cc307add487163ffed48
Mike de la Fuente <mike.delafuente@gmail.com>
2021-05-31 10:44:56 -0700

    Remove Student from sample index page

--- signin/internal/controllers/index.go
+++ signin/internal/controllers/index.go
@@ -8,18 +8,7 @@ import (
 	"github.com/mikedelafuente/authful-servertools/pkg/logger"
 )
 
-type Student struct {
-	Name       string
-	College    string
-	RollNumber int
-}
-
 func Index(w http.ResponseWriter, r *http.Request) {
-	student := Student{
-		Name:       "GB",
-		College:    "GolangBlogs",
-		RollNumber: 1,
-	}
 	parsedTemplate, _ := template.ParseFiles("Templates/index.html")
 	err := parsedTemplate.Execute(w, student)
 	if err != nil {
