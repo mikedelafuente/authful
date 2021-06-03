c91e0caa4e09275ee47aaf67a31439036e00a462
Mike de la Fuente <mike.delafuente@gmail.com>
2021-06-03 11:56:59 -0700

    Update styling for login

--- web/src/components/HelloWorld.vue
+++ web/src/components/HelloWorld.vue
@@ -1,68 +1,104 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-     <button v-on:click="reverseMessage">Reverse Message</button>
+  <div class="px-2 py-2 my-2 text-center">
+    <div class="display-5 fw-bold">{{ msg }}</div>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-3">
+        For a guide and recipes on how to configure / customize this project,
+        <br />check out the
+        <a
+          href="https://cli.vuejs.org"
+          target="_blank"
+          rel="noopener"
+        >vue-cli documentation</a>.
+      </p>
+    </div>
   </div>
+  <div class="px-2 py-2 my-2 text-center">
+    <div class="display-6 fw-bold">Installed CLI Plugins</div>
+    <div class="col-lg-6 mx-auto">
+      <ul>
+        <li>
+          <a
+            href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+            target="_blank"
+            rel="noopener"
+          >babel</a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
+            target="_blank"
+            rel="noopener"
+          >eslint</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class="px-2 py-1 my-1 text-center">
+    <div class="display-6 fw-bold">Essential Links</div>
+    <div class="col-lg-6 mx-auto">
+      <ul>
+        <li>
+          <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
+        </li>
+        <li>
+          <a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a>
+        </li>
+        <li>
+          <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a>
+        </li>
+        <li>
+          <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a>
+        </li>
+        <li>
+          <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="px-2 py-2 my-2 text-center">
+    <div class="display-6 fw-bold">Ecosystem</div>
+    <div class="col-lg-6 mx-auto">
+      <ul>
+        <li>
+          <a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a>
+        </li>
+        <li>
+          <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/vuejs/vue-devtools#vue-devtools"
+            target="_blank"
+            rel="noopener"
+          >vue-devtools</a>
+        </li>
+        <li>
+          <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a>
+        </li>
+        <li>
+          <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <button class="btn btn-primary btn-lg px-4 gap-3" v-on:click="reverseMessage">Reverse Message</button>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
+    msg: String,
   },
   methods: {
-     reverseMessage: function () {
-      const axios = require('axios').default;
-
-      this.$.props.msg = this.$.props.msg.split('').reverse().join('');
-      axios.post('http://localhost:8081/api/v1/signin', {
-        username:'try',
-        password:'this'
-      },{
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          Cache: "no-cache",
-        },
-        withCredentials: true
-      }
-      ).then(response => {
-                this.$cookies.set("userJwt", response.data.jwt, new Date(response.data.expires))
-                //this.response = JSON.stringify(response, null, "")
-            }).catch(error => {
-                console.log(error);
-            });
-
-    }
-  }
-}
+    reverseMessage: function () {
+      this.$.props.msg = this.$.props.msg.split("").reverse().join("");     
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- web/src/main.js
+++ web/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import VueCookies from 'vue3-cookies'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap'
 
  createApp(App).use(router).use(VueCookies).mount('#app')
