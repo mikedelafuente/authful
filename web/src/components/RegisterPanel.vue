4f66d44a1179c597d12240cafb59624b3efec926
Mike de la Fuente <mike.delafuente@keap.com>
2021-06-05 19:16:44 -0700

    Working through password matching

--- web/src/components/RegisterPanel.vue
+++ web/src/components/RegisterPanel.vue
@@ -1,7 +1,20 @@
 <template>
   <form
-    class="form-register col-5 my-3 px-3 align-items-center rounded-3 border shadow-lg needs-validation" novalidate  @submit="checkForm"
+    class="
+      form-register
+      col-5
+      my-3
+      px-3
+      align-items-center
+      rounded-3
+      border
+      shadow-lg
+      needs-validation
+    "
+    novalidate
+    @submit="checkForm"
   >
+
     <img
       class="mb-4"
       src="../assets/WeekendClip.png"
@@ -15,15 +28,14 @@
         <input
           type="email"
           class="form-control"
-          id="floatingInput"
+          id="email"
           v-model="email"
           placeholder="name@example.com"
           required
+          @input="passwordCheck"
         />
         <label for="floatingInput">Email address</label>
-         <div class="invalid-feedback">
-      Email address is required
-    </div>
+        <div class="invalid-feedback">Email address is required</div>
       </div>
     </div>
     <div class="form-group">
@@ -31,31 +43,31 @@
         <input
           type="password"
           class="form-control"
-          id="floatingPassword"
+          v-bind:class="[isPasswordMatch ? validClass : invalidClass ]"
+          id="password1"
           v-model="password"
           placeholder="Password"
           required
+          @input="validatePasswords"
         />
         <label for="floatingPassword">Password</label>
       </div>
       <div class="form-floating py-1">
         <input
-          type="password"
+           type="password"
           class="form-control"
-          id="floatingConfirmPassword"
+          v-bind:class="[isPasswordMatch ? validClass : invalidClass ]"
+          id="password1"
           v-model="confirmPassword"
-          placeholder="Re-enter password"
+          placeholder="Password"
           required
+          @input="validatePasswords"
         />
         <label for="floatingConfirmPassword">Re-enter password</label>
-       
       </div>
     </div>
     <div class="form-group py-3">
-      <button
-        class="w-100 btn btn-lg btn-primary"
-        type="submit"
-      >
+      <button class="w-100 btn btn-lg btn-primary disabled" ref="createAccountButton" type="submit">
         Register
       </button>
       <div class="mt-2 mb-3">
@@ -72,33 +84,71 @@ export default {
   name: "LoginPanel",
   data() {
     return {
+      validClass: "is-valid",
+      invalidClass: "",
       email: "",
       password: "",
       confirmPassword: "",
+      isPasswordMatch: false,
     };
   },
   props: {},
   methods: {
+     checkForm: function (e) {
+      this.errors = [];
+
+      if (!this.email) {
+        this.errors.push('Email required.');
+      } else if (!this.validEmail(this.email)) {
+        this.errors.push('Valid email required.');
+      }
+
+      this.validatePasswords();
+      if (this.isPasswordMatch)
+      if (!this.errors.length) {
+        return true;
+      }
+
+      e.preventDefault();
+    },
+    validEmail: function (email) {
+      var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    },
+    validatePasswords: function () {
+      if (this.password != this.confirmPassword || this.password == '' || this.confirmPassword == '') {
+        this.isPasswordMatch = false;
+         if (this.password == '') {
+          this.invalidClass = "";
+        } else {
+          this.invalidClass = "is-invalid";
+        }
+      } else {
+        this.isPasswordMatch = true;       
+      }
+    },
     // global function
-    checkForm: function(e) {
+    checkForm2: function (e) {
       var form = document.querySelector(`form`);
-  // Loop over them and prevent submission
+      // Loop over them and prevent submission
       console.log(e);
       if (!form.checkValidity()) {
-        console.log("not valid")
+        console.log("not valid");
         e.preventDefault();
-        e.target.checkValidity();      
+        e.target.checkValidity();
       } else {
-        console.log("valid")
+        this.passwordCheck();
+
+        if (this.isPasswordMatch == false) {
+          e.preventDefault();
+          e.target.checkValidity();
+
+          console.log("password mismatch");
+        } else {
+          console.log("valid");
+        }
       }
-       form.classList.add('was-validated')
-    //  if (!form.checkValidity()) {
-    //       e.preventDefault()
-    //       e.stopPropagation()
-    //     } else {
-    //         e.preventDefault()
-    //         e.stopPropagation()       
-    //     }
+      form.classList.add("was-validated");
     },
     doRegister: function () {
       const axios = require("axios").default;
